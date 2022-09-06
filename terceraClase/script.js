68f5f158ec5a035ba025187f72756fe4338a1e68
nico <sagasteguinicolas69@gmail.com>
2022-09-06 17:59:02 -0300

    tercera clases

---
+++ terceraClase/script.js
@@ -0,0 +1,43 @@
+console.log("Hola");
+
+// variables
+
+var x = 41;
+var y = 59;
+
+var suma = x + y;
+
+var resta = x - y;
+
+var div = x / y;
+
+var mult = x * y;
+
+// tipos de datos
+//String
+//num(int-float)
+//null
+//underfinal
+
+//String
+var nombre = "alan";
+nombre = "juan";
+//num
+var edad = 27;
+
+//bool(true o false)
+var casado = false;
+
+//undefined (no definido)
+// null (vacio)
+var pepe = null;
+
+// bool(son la respuesta a una condicion)
+
+// > mayor a que
+var cond = 10 > 5; // true
+
+// < menor a que 
+var cod2 = 20 < 50; // false
+
+var respuesta = prompt("Hola Alan");
